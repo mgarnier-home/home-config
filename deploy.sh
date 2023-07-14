3094028f2b7e4aa661cbb59643638bbd8944d467
Mathieu GARNIER <mgarnier11@gmail.com>
2023-07-15 00:32:16 +0100

    feat: improvments to stack, added the samba stack

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 set -o allexport
 source ./.env
 
-docker stack config -c docker-compose.yml
-docker stack deploy -c docker-compose.yml home
+# docker stack config -c home.yml
+docker stack deploy -c home.yml home
+
+# docker stack config -c samba.yml
+docker stack deploy -c samba.yml samba
 
 set +o allexport
