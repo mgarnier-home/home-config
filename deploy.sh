38ef444163a8a5bf96e91c9250981dc0d3fc6c3e
Mathieu GARNIER <mgarnier11@gmail.com>
2023-07-16 00:43:11 +0100

    feat: added the backup stack

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,7 @@ docker stack deploy -c home.yml home
 # docker stack config -c samba.yml
 docker stack deploy -c samba.yml samba
 
+# docker stack config -c backup.yml
+docker stack deploy -c backup.yml backup
+
 set +o allexport
