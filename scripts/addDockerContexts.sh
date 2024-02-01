892cd149da7d1cad9270110e9a17f468a559cc50
Mathieu GARNIER <mgarnier11@gmail.com>
2024-02-01 20:20:00 +0100

    feat: removed linking to ~/docker-configs, updated tz env variable

--- scripts/addDockerContexts.sh
+++ scripts/addDockerContexts.sh
@@ -1,4 +1,6 @@
-cd ~/docker-configs
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/..
 
 set -o allexport
 source ./.env
--- scripts/my-stack.sh
+++ scripts/my-stack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd ~/docker-configs
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/..
 
 set -o allexport
 source ./.env
